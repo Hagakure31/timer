72971116e64ce95110936d852bd2c8e8a2a5d918
Hagakure31 <luffyeic@gmail.com>
2019-06-10 19:37:29 +0200

    modif

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// function input() {
+// document.getElementById("dateInput").addEventListener("change", function(){
+//     var input = this.value;
+//     var dateEntered = new Date(input);
+
+//     return dateEntered;
+// });
+
+
+
+
+function countdown() {
+    
+    var now = new Date();
+    var eventDate = new Date('September 12, 2019');
+    
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+    
+    var remainingTime = eventTime - currentTime;
+
+    var s = Math.floor(remainingTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d= Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+
+
+    document.getElementById('days').textContent = d;
+    document.getElementById('hours').textContent = h;
+    document.getElementById('minutes').textContent = m;
+    document.getElementById('seconds').textContent = s;
+
+    setTimeout(countdown, 1000);
+}
+
+countdown();
